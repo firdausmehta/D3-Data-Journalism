f17937776660d5a9b91f8638e17ad040aaa69116
Firdaus Mehta <firmehta97@gmail.com>
2021-08-24 20:01:25 +1000

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -10,3 +10,36 @@ let margin = {
   bottom: 200,
   left: 100
 };
+
+// calculate chart height and width
+let width = svgWidth - margin.right - margin.left;
+let height = svgHeight - margin.top - margin.bottom;
+
+// append a div class to the scatter element
+let chart = d3.select('#scatter')
+  .append('div')
+  .classed('chart', true);
+
+//append an svg element to the chart 
+let svg = chart.append('svg')
+  .attr('width', svgWidth)
+  .attr('height', svgHeight);
+
+//append an svg group
+let chartGroup = svg.append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+//initial parameters; x and y axis
+let chosenXAxis = 'poverty';
+let chosenYAxis = 'healthcare';
+
+//a function for updating the x-scale variable upon click of label
+function xScale(censusData, chosenXAxis) {
+    //scales
+    let xLinearScale = d3.scaleLinear()
+      .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.8,
+        d3.max(censusData, d => d[chosenXAxis]) * 1.2])
+      .range([0, width]);
+
+    return xLinearScale;
+}
