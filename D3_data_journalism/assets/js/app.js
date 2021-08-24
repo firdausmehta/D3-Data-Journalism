688af4fe93f3f02a0859c882b0975d98dff67bb3
Firdaus Mehta <firmehta97@gmail.com>
2021-08-24 18:52:31 +1000

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,12 @@
-// @TODO: YOUR CODE HERE!
+// Level 1 Requirement, D3:  Create a scatter plot for visualization of data based on the 2014 ACS 1 year estimates.  
+// Set up SVG definitions
+let svgWidth = 960;
+let svgHeight = 620;
+
+// set up borders in svg
+let margin = {
+  top: 20, 
+  right: 40, 
+  bottom: 200,
+  left: 100
+};
